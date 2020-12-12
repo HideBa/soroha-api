cdd717eb35c6b6be20fe44df3baefdda01250994
HideBa <baba.papa1120.ba@gmail.com>
2020-12-12 16:30:53 +0900

    fix config error

--- db/db.go
+++ db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/HideBa/soroha-api/config"
@@ -14,9 +13,9 @@ var db *gorm.DB
 var err error
 
 func Init() {
-	cfg := config.GetConfig()
-	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", cfg.DB.DbUser, cfg.DB.DbPass, cfg.DB.DbHost, cfg.DB.DbPort, cfg.DB.DbName)
-	fmt.Println("url", url)
+	// cfg := config.GetConfig()
+	// url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", cfg.DB.DbUser, cfg.DB.DbPass, cfg.DB.DbHost, cfg.DB.DbPort, cfg.DB.DbName)
+	// fmt.Println("url", url)
 	// db, err = gorm.Open("mysql", url)
 	db, err = gorm.Open("mysql", config.GetConfig().DB.DbURL)
 	if err != nil {
